b9b632eb0b72ccca461d459bdf7cc734dad88e20
Elise3993 <84709045+Elise3993@users.noreply.github.com>
2024-07-14 03:10:19 +0900

    APIのimport pathを修正

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,13 +1,23 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
-import app from "./api/api.mjs";
-import { CommandDefine, ModalDefine, ReceiveCommand, ReceiveModal } from "./command/add.mjs";
+import app from "./api/events.mjs";
+
+import {
+  CommandDefine,
+  ModalDefine,
+  ReceiveCommand,
+  ReceiveModal,
+} from "./command/add.mjs";
 dotenv.config();
 // import { fs } from 'fs';
 // const token = JSON.parse(fs.readFileSync('../config.json', 'utf-8')).token;
 const { token } = process.env;
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 client.once(Events.ClientReady, async (readyClient) => {
@@ -36,6 +46,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
 });
 client.login(token);
 
+// APIサーバーを起動
 const PORT = 4000;
 app.listen(4000, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
