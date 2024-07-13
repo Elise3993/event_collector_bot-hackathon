6db44cf818590a77e5f49cc75a0affa449074073
ToYama170402 <100946113+ToYama170402@users.noreply.github.com>
2024-07-13 20:01:00 +0900

    add.mjsのエクスポートを改善。 新しいコマンドを追加する時に名前の衝突を避けるため。

--- backend/src/command/add.mjs
+++ backend/src/command/add.mjs
@@ -1,8 +1,8 @@
-import { CommandDefine } from './add/CommandDefine.mjs';
-import { ModalDefine } from './add/ModalDefine.mjs';
-import { ReceiveCommand } from './add/ReceiveCommand.mjs';
-import { ReceiveModal } from './add/ReceiveModal.mjs';
-export {
+import { CommandDefine } from "./add/CommandDefine.mjs";
+import { ModalDefine } from "./add/ModalDefine.mjs";
+import { ReceiveCommand } from "./add/ReceiveCommand.mjs";
+import { ReceiveModal } from "./add/ReceiveModal.mjs";
+export const addCommand = {
   CommandDefine,
   ModalDefine,
   ReceiveCommand,
--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,7 +1,7 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
 import app from "./api/api.mjs";
-import { CommandDefine, ModalDefine, ReceiveCommand, ReceiveModal } from "./command/add.mjs";
+import { addCommand } from "./command/add.mjs";
 dotenv.config();
 // import { fs } from 'fs';
 // const token = JSON.parse(fs.readFileSync('../config.json', 'utf-8')).token;
@@ -13,7 +13,7 @@ const client = new Client({
 client.once(Events.ClientReady, async (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
   try {
-    await client.application.commands.set(CommandDefine, "1259019648591204465");
+    await client.application.commands.set([addCommand.CommandDefine], "1259019648591204465");
     console.log("Slash Commands Registered!");
   } catch (err) {
     console.error(err);
@@ -25,11 +25,11 @@ client.on(Events.InteractionCreate, async (interaction) => {
       console.log("add command triggered!");
       if (interaction.options._hoistedOptions.length === 0) {
         console.log("Form option selected!");
-        interaction.showModal(ModalDefine);
+        interaction.showModal(addCommand.ModalDefine);
       }
-      ReceiveCommand(interaction);
+      addCommand.ReceiveCommand(interaction);
     }
-    ReceiveModal(interaction);
+    addCommand.ReceiveModal(interaction);
   } catch (err) {
     console.error(err, interaction);
   }
