26a584535b1f7c49a0502dc4b396abbf022b4a83
ToYama170402 <100946113+ToYama170402@users.noreply.github.com>
2024-07-13 11:12:00 +0900

    8 discord botの開発環境の移行 (#17)
    * dotenvパッケージを追加
    Discord BotのToKenを設定する.envファイルを読み込むため
    
    * この環境に移行するまで書いていたコードを移行。
    
    * .env.sampleのパスを変更
    元のパスでは動作しないから。
    
    * コマンドsetするサーバーを1つ削除
    
    ---------
    
    Co-authored-by: Elise <84709045+Elise3993@users.noreply.github.com>

---
+++ backend/src/command/add.mjs
@@ -0,0 +1,10 @@
+import { CommandDefine } from './add/CommandDefine.mjs';
+import { ModalDefine } from './add/ModalDefine.mjs';
+import { ReceiveCommand } from './add/ReceiveCommand.mjs';
+import { ReceiveModal } from './add/ReceiveModal.mjs';
+export {
+  CommandDefine,
+  ModalDefine,
+  ReceiveCommand,
+  ReceiveModal,
+};
--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,31 +1,45 @@
-// Nodeの動作テスト
-// discordの機能入れて勝手に変更していいよ
-import http from "http";
-import url from "url";
-import createDB from "./db_create_test.mjs";
-import insertDB from "./db_insert_test.mjs";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+import {
+  CommandDefine,
+  ModalDefine,
+  ReceiveCommand,
+  ReceiveModal,
+} from "./command/add.mjs";
+dotenv.config();
+// import { fs } from 'fs';
+// const token = JSON.parse(fs.readFileSync('../config.json', 'utf-8')).token;
+const { token } = process.env;
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
-const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url);
-  const pathname = parsedUrl.pathname;
-
-  if (pathname === "/") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello, World!\n");
-    console.log("ok");
-  } else if (pathname === "/create_table") {
-    createDB();
-
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("created table!\n");
-  } else if (pathname === "/insert_data") {
-    insertDB();
-
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("inserted data!\n");
+client.once(Events.ClientReady, async (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+  try {
+    await client.application.commands.set(CommandDefine, "1259019648591204465");
+    console.log("Slash Commands Registered!");
+  } catch (err) {
+    console.error(err);
   }
 });
-
-server.listen(4000, () => {
-  console.log("Server running at http://localhost:4000/");
+client.on(Events.InteractionCreate, async (interaction) => {
+  try {
+    if (interaction.commandName === "add") {
+      console.log("add command triggered!");
+      if (interaction.options._hoistedOptions.length === 0) {
+        console.log("Form option selected!");
+        interaction.showModal(ModalDefine);
+      }
+      ReceiveCommand(interaction);
+    }
+    ReceiveModal(interaction);
+  } catch (err) {
+    console.error(err, interaction);
+  }
 });
+client.login(token);
