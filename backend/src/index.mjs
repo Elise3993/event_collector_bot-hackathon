c67665c2165cd0eeadb21a271c571ee1c95e689a
Elise3993 <84709045+Elise3993@users.noreply.github.com>
2024-07-13 11:11:13 +0900

    コマンドsetするサーバーを1つ削除

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,6 +1,11 @@
-import { Client, GatewayIntentBits, Events } from 'discord.js';
-import { CommandDefine, ModalDefine, ReceiveCommand, ReceiveModal } from './command/add.mjs';
-import dotenv from 'dotenv';
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+import {
+  CommandDefine,
+  ModalDefine,
+  ReceiveCommand,
+  ReceiveModal,
+} from "./command/add.mjs";
 dotenv.config();
 // import { fs } from 'fs';
 // const token = JSON.parse(fs.readFileSync('../config.json', 'utf-8')).token;
@@ -9,23 +14,22 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-client.once(Events.ClientReady, async readyClient => {
+client.once(Events.ClientReady, async (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
   try {
-    await client.application.commands.set(CommandDefine, '956804038836518942');
-    await client.application.commands.set(CommandDefine, '1259019648591204465');
+    await client.application.commands.set(CommandDefine, "1259019648591204465");
     console.log("Slash Commands Registered!");
   } catch (err) {
     console.error(err);
   }
 });
-client.on(Events.InteractionCreate, async interaction => {
+client.on(Events.InteractionCreate, async (interaction) => {
   try {
-    if (interaction.commandName === 'add') {
+    if (interaction.commandName === "add") {
       console.log("add command triggered!");
       if (interaction.options._hoistedOptions.length === 0) {
         console.log("Form option selected!");
@@ -34,6 +38,8 @@ client.on(Events.InteractionCreate, async interaction => {
       ReceiveCommand(interaction);
     }
     ReceiveModal(interaction);
-  } catch (err) { console.error(err, interaction); }
+  } catch (err) {
+    console.error(err, interaction);
+  }
 });
 client.login(token);
